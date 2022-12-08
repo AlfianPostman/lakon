682de873eae0514f9754f008ad95eb0bf9865e21
AlfianPostman <kinobabes@gmail.com>
2022-12-08 15:45:45 +0700

    [FIX] something

--- Assets/Scripts/Pathfinding.cs
+++ Assets/Scripts/Pathfinding.cs
@@ -54,6 +54,8 @@ public class Pathfinding : MonoBehaviour {
 						
 						if (!openSet.Contains(neighbour))
 							openSet.Add(neighbour);
+                        else
+                            openSet.UpdateItem(neighbour);
 					}
 				}
 			}
